bc48d1fd0d829b0947c0cda9409a78dfc0359930
PatoBeltran <pbelsal@gmail.com>
2015-05-03 00:01:03 -0500

    Pls work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { :host => 'mi2d.herokuapp.com' }
+
   # Email Setup
   EMAIL_CREDENTIALS = {
     username: "patobeltran",
