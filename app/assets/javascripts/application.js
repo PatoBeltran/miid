decff53e53d6d2e8982bbe50390c01b3b7a5d063
Eduardo <edig50@gmail.com>
2015-05-02 20:11:45 -0500

    OJT

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -221,6 +221,15 @@ function searchCourse(q) {
                     '</a>');
 
                 $('#courses').append(newChild);
+
+                    $("a", $("#gallery")).draggable({
+                       //                cancel: "a.ui-icon", // clicking an icon won't initiate dragging
+                        revert: "invalid", // when not dropped, the item will revert back to its initial position
+                        containment: "document",
+                        helper: "clone",
+                        cursor: "move"
+                  });
+
             });
 
 
