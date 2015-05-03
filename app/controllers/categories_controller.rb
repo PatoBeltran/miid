3ba2980a3c31e3ec8930c07372bfa49b3eb6494e
PatoBeltran <pbelsal@gmail.com>
2015-05-03 00:20:35 -0500

    change redirection

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,7 +9,7 @@ class CategoriesController < ApplicationController
     @category = Category.new(category_params)
 
     if @category.save
-      redirect_to root_path
+      redirect_to categories_path
     else
       render :new
     end
